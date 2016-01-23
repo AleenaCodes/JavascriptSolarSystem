68be9464ea9f3eea340e98ae032ae963e5adbb1e
AleenaCodes <aleena@hotmail.co.uk>
2016-01-23 15:37:04 +0000

    Added own sun pic 7

--- solarSystem.js
+++ solarSystem.js
@@ -22,14 +22,14 @@
     // Earth
     var time = new Date();
     ctx.rotate( ((2*Math.PI)/60)*time.getSeconds() + ((2*Math.PI)/60000)*time.getMilliseconds() );
-    ctx.translate(105,0);
+    ctx.translate(210,0);
     //ctx.fillRect(0,-12,50,24); // Shadow
     ctx.drawImage(earth,-12,-12);
 
     // Moon
     ctx.save();
     ctx.rotate( ((2*Math.PI)/6)*time.getSeconds() + ((2*Math.PI)/6000)*time.getMilliseconds() );
-    ctx.translate(0,28.5);
+    ctx.translate(0,57);
     ctx.drawImage(moon,-3.5,-3.5);
     ctx.restore();
 
