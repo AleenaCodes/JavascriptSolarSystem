cd5978778e3c5c331f3c3cefe8a8c1bd2775c0c3
AleenaCodes <aleena@hotmail.co.uk>
2016-01-23 15:23:13 +0000

    Added own sun pic 3

--- solarSystem.js
+++ solarSystem.js
@@ -12,7 +12,7 @@
     var ctx = document.getElementById('canvas').getContext('2d');
 
     ctx.globalCompositeOperation = 'destination-over';
-    ctx.clearRect(0,0,300,300); // clear canvas
+    ctx.clearRect(0,0,600,600); // clear canvas
 
     ctx.fillStyle = 'rgba(0,0,0,0.4)';
     ctx.strokeStyle = 'rgba(0,153,255,0.4)';
