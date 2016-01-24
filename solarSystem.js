a6382e8e43cb8b9c98fb0e8e3b319841ffa099fe
AleenaCodes <aleena@hotmail.co.uk>
2016-01-24 11:11:37 +0000

    Changing Earth Orbit 1

--- solarSystem.js
+++ solarSystem.js
@@ -1,47 +1,48 @@
-    var sun = new Image();
-    var moon = new Image();
-    var earth = new Image();
-    function init(){
+
+  var sun = new Image();
+  var moon = new Image();
+  var earth = new Image();
+  function init(){
     sun.src = 'sun.png';
     moon.src = 'https://mdn.mozillademos.org/files/1443/Canvas_moon.png';
     earth.src = 'https://mdn.mozillademos.org/files/1429/Canvas_earth.png';
     window.requestAnimationFrame(draw);
-    }
+  }
 
-    function draw() {
-    var ctx = document.getElementById('canvas').getContext('2d');
+  function draw() {
+  var ctx = document.getElementById('canvas').getContext('2d');
 
-    ctx.globalCompositeOperation = 'destination-over';
-    ctx.clearRect(0,0,600,600); // clear canvas
+  ctx.globalCompositeOperation = 'destination-over';
+  ctx.clearRect(0,0,600,600); // clear canvas
 
-    ctx.fillStyle = 'rgba(0,0,0,0.4)';
-    ctx.strokeStyle = 'rgba(0,153,255,0.4)';
-    ctx.save();
-    ctx.translate(300,300);
+  ctx.fillStyle = 'rgba(0,0,0,0.4)';
+  ctx.strokeStyle = 'rgba(0,153,255,0.4)';
+  ctx.save();
+  ctx.translate(150,150);
 
-    // Earth
-    var time = new Date();
-    ctx.rotate( ((2*Math.PI)/60)*time.getSeconds() + ((2*Math.PI)/60000)*time.getMilliseconds() );
-    ctx.translate(210,0);
-    //ctx.fillRect(0,-12,50,24); // Shadow
-    ctx.drawImage(earth,-12,-12);
+  // Earth
+  var time = new Date();
+  ctx.rotate( ((2*Math.PI)/60)*time.getSeconds() + ((2*Math.PI)/60000)*time.getMilliseconds() );
+  ctx.translate(210,0);
+  //ctx.fillRect(0,-12,50,24); // Shadow
+  ctx.drawImage(earth,-12,-12);
 
-    // Moon
-    //ctx.save();
-    //ctx.rotate( ((2*Math.PI)/6)*time.getSeconds() + ((2*Math.PI)/6000)*time.getMilliseconds() );
-    //ctx.translate(0,57);
-    //ctx.drawImage(moon,-3.5,-3.5);
-    //ctx.restore();
+  // Moon
+  //ctx.save();
+  //ctx.rotate( ((2*Math.PI)/6)*time.getSeconds() + ((2*Math.PI)/6000)*time.getMilliseconds() );
+  //ctx.translate(0,57);
+  //ctx.drawImage(moon,-3.5,-3.5);
+  //ctx.restore();
 
-    ctx.restore();
+  ctx.restore();
 
-    ctx.beginPath();
-    ctx.arc(300,300,210,0,Math.PI*2,false); // Earth orbit
-    ctx.stroke();
+  ctx.beginPath();
+  ctx.arc(300,300,210,0,Math.PI*2,false); // Earth orbit
+  ctx.stroke();
 
-    ctx.drawImage(sun,0,0,600,600);
+  ctx.drawImage(sun,0,0,600,600);
 
-    window.requestAnimationFrame(draw);
-    }
+  window.requestAnimationFrame(draw);
+  }
 
-    init();
+  init();
